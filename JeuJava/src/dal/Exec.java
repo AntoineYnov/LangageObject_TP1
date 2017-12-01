d67c0014b21724497aa2b55938d8ef2f665f0167
Antoine Genouin-Duhamel <antoine.genouinduhamel@ynov.com>
2017-12-01 14:17:04 +0100

    combat jeu

--- JeuJava/src/dal/Exec.java
+++ JeuJava/src/dal/Exec.java
@@ -1,15 +1,17 @@
 package dal;
 
 import dao.MonstreDAO;
+import metier.Jeu;
 
 public class Exec {
 
 	public static void main(String[] args) {
+		Jeu jeu = new Jeu(	Monstre monstre1,Monstre monstre2);
 		MonstreDAO monstre = new MonstreDAO();
 		
-		monstre.select();
-		//monstre.getById(1);
-		//monstre.insert();
+//		monstre.select();
+//		monstre.getById(1);
+		monstre.insert();
 
 	}
 
--- JeuJava/src/metier/Jeu.java
+++ JeuJava/src/metier/Jeu.java
@@ -5,4 +5,12 @@ import presentation.Monstre;
 public class Jeu {
 	Monstre monstre1, monstre2;
 	boolean jeu=true;
+	
+	public Jeu(Monstre monstre1,Monstre monstre2) {
+		this.monstre1=monstre1;
+		this.monstre1=monstre2;
+	}
+	public int combat (Monstre monstre1,Monstre monstre2) {
+		return 0;
+	}
 }
